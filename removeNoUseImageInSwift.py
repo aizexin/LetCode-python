57f56d55f303b540ca2f1c56694eaac0488d4a9a
Lifeiyu <lifeiyu@address.com>
2023-04-20 17:33:22 +0800

    Update removeNoUseImageInSwift.py

--- removeNoUseImageInSwift.py
+++ removeNoUseImageInSwift.py
@@ -2,7 +2,7 @@ import os , shutil
 from typing import List
 from collections import defaultdict
 
-# 获取图片名字返回名字数组 
+# 获取图片名字返回名字数组
 def getImagePath(basePath, dirPath, pathList) -> List[str]:
     if len(dirPath) > 0:
         imagesPath = os.path.join(basePath, dirPath)
@@ -12,9 +12,11 @@ def getImagePath(basePath, dirPath, pathList) -> List[str]:
     for fileName in allPath:
         currentDirPath = os.path.join(dirPath, fileName)
         fullPath = os.path.join(basePath, currentDirPath)
+        if 'ZesayIm' in fileName:
+            continue
         # 如果是.imageset 直接加入
         if fileName.endswith('.imageset'):
-            imageName = currentDirPath.replace('.imageset','')
+            imageName = '"' + currentDirPath.replace('.imageset','') + '"'
             pathList.append(imageName)
             continue
         # 如果是文件夹
@@ -33,8 +35,9 @@ def readSingleFile(basePath, dirPath, fileName, pathDict) -> defaultdict:
     while True:
         line :str = readF.readline()
         if line:
+            lineString = str(line)
             for key in pathDict:
-                if key in line:
+                if key in lineString:
                     pathDict[key] += 1
         else:
             break
@@ -60,6 +63,15 @@ def lookSingleDir(basePath, dirPath, pathDict) -> defaultdict:
                 pathDict = readSingleFile(basePath,dirPath,fileName,pathDict)
     return pathDict
 
+def writeToFile(array, path):
+    if not os.path.exists(path):
+        os.mknod(tempImagePathFile)
+        print('写入文件不存在')
+    writeF = open(path,'w+')
+    for line in array:
+        writeF.write(line + '\n')
+    writeF.close()
+
 if __name__ == '__main__':
     basePath = os.getcwd()
     # 获得图片路经名字
@@ -77,14 +89,19 @@ if __name__ == '__main__':
     if os.path.exists(noUseImagePath) == False:
         os.mkdir(noUseImagePath)
     
+    noUseArray = list()
     for key in pathDict:
         if pathDict[key] == 0:
             # 这些初步认为是没有用到的图片
-            fullPath = os.path.join(imageBasePath, key) + '.imageset'
-            toPath = os.path.join(noUseImagePath, key) + '.imageset'
-            if os.path.exists(toPath) == False:
-                # os.makedirs(toPath)
+            noUseArray.append(key)
+            imageName = str(key).replace('"','')
+            fullPath = os.path.join(imageBasePath, imageName) + '.imageset'
+            toPath = os.path.join(noUseImagePath, imageName) + '.imageset'
+            if os.path.exists(fullPath):
                 shutil.move(fullPath,toPath)
     print('移除完成...')
+    tempImagePathFile = os.path.join(basePath,'pathImage.txt')
+    writeToFile(noUseArray, tempImagePathFile)
+    print('未使用的图片名字写入txt完成...')
             
         
